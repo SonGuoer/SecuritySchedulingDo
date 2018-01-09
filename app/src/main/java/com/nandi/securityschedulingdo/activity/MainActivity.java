6ef0d2465960306b9af3ce7cf2d9bf983c3d5b15
qingsong <1154046205@qq.com>
2018-01-09 17:18:25 +0800

    搜索

--- app/src/main/java/com/nandi/securityschedulingdo/activity/MainActivity.java
+++ app/src/main/java/com/nandi/securityschedulingdo/activity/MainActivity.java
@@ -1,8 +1,17 @@
 package com.nandi.securityschedulingdo.activity;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
@@ -21,10 +30,17 @@ import com.baidu.mapapi.map.Overlay;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.google.gson.Gson;
+import com.nandi.securityschedulingdo.Constant;
 import com.nandi.securityschedulingdo.R;
+import com.nandi.securityschedulingdo.adapter.AreaAdapter;
 import com.nandi.securityschedulingdo.bean.AreaName;
+import com.nandi.securityschedulingdo.utils.JsonFormat;
+import com.nandi.securityschedulingdo.utils.SharedUtils;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,12 +52,22 @@ public class MainActivity extends AppCompatActivity {
     private MapView mMapView;
     private BitmapDescriptor mCurrentMarker;
     private BaiduMap mBaidumap;
+    private Spinner areaName;
+    private Spinner typePoint;
+    private List<AreaName> areaNames;
+    private int typeId;
+    private int areaId;
+    private ImageView search;
+    private int level;
+    private TextView cityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         areaRequest();
+        level = (int) SharedUtils.getShare(this, Constant.LEVEL, 1);
+        areaId = (int) SharedUtils.getShare(this, Constant.AREA_ID, 0);
         initMapView();
         initOverLay();
 //        mBaidumap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
@@ -68,8 +94,34 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response, int id) {
                         System.out.println("response = " + response);
-                        Gson gson = new Gson();
-                        AreaName areaName = gson.fromJson(response, AreaName.class);
+                        JSONObject js = null;
+                        try {
+                            js = new JSONObject(response);
+                            JSONObject meta = new JSONObject(js.optString("meta"));
+                            Boolean success = meta.optBoolean("success");
+                            String message = meta.optString("message");
+                            if (success) {
+                                String data = js.optString("data");
+                                areaNames = new ArrayList<>();
+                                areaNames.add(new AreaName(0, 0, 0, 0, 0, "全部", 0, 0));
+                                areaNames.addAll(JsonFormat.stringToList(data, AreaName.class));
+                                AreaAdapter areaAdapter = new AreaAdapter(areaNames, MainActivity.this);
+                                areaName.setAdapter(areaAdapter);
+                                if (level > 1) {
+                                    for (int i = 0; i < areaNames.size(); i++) {
+                                        if (areaId == areaNames.get(i).getId()) {
+                                          areaName.setSelection(i);
+                                          areaName.setEnabled(false);
+                                        }
+                                    }
+                                }
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
                     }
                 });
     }
@@ -84,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
     private void initMapView() {
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
+
+        initListener();
         mBaidumap = mMapView.getMap();
         mBaidumap.setMyLocationEnabled(true);
         // 构造定位数据
@@ -105,6 +159,46 @@ public class MainActivity extends AppCompatActivity {
         mBaidumap.setMyLocationConfiguration(config);
         // 当不需要定位图层时关闭定位图层
         mBaidumap.setMyLocationEnabled(false);
+    }
+
+    private void initListener() {
+        search = findViewById(R.id.search_point);
+        cityName = findViewById(R.id.city_name);
+        areaName = findViewById(R.id.area_name);
+        typePoint = findViewById(R.id.type_point);
+        areaName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    areaId = 1;
+                } else {
+                    areaId = areaNames.get(position).getId();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        typePoint.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                typeId = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/nandi/securityschedulingdo/bean/AreaName.java
+++ app/src/main/java/com/nandi/securityschedulingdo/bean/AreaName.java
@@ -7,86 +7,98 @@ import java.util.List;
  */
 
 public class AreaName {
-        /**
-         * id : 2
-         * lon : 108.426777
-         * level : 3
-         * area_parent : 1
-         * area_code : 500101
-         * area_name : 万州区
-         * unit_id : 2
-         * lat : 30.714754
-         */
-        private int id;
-        private double lon;
-        private int level;
-        private int area_parent;
-        private int area_code;
-        private String area_name;
-        private int unit_id;
-        private double lat;
+    /**
+     * id : 2
+     * lon : 108.426777
+     * level : 3
+     * area_parent : 1
+     * area_code : 500101
+     * area_name : 万州区
+     * unit_id : 2
+     * lat : 30.714754
+     */
+    private int id;
+    private double lon;
+    private int level;
+    private int area_parent;
+    private int area_code;
+    private String area_name;
+    private int unit_id;
+    private double lat;
 
-        public int getId() {
-            return id;
-        }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    public AreaName(int id, double lon, int level, int area_parent, int area_code, String area_name, int unit_id, double lat) {
+        this.id = id;
+        this.lon = lon;
+        this.level = level;
+        this.area_parent = area_parent;
+        this.area_code = area_code;
+        this.area_name = area_name;
+        this.unit_id = unit_id;
+        this.lat = lat;
+    }
 
-        public double getLon() {
-            return lon;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public void setLon(double lon) {
-            this.lon = lon;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public int getLevel() {
-            return level;
-        }
+    public double getLon() {
+        return lon;
+    }
 
-        public void setLevel(int level) {
-            this.level = level;
-        }
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
 
-        public int getArea_parent() {
-            return area_parent;
-        }
+    public int getLevel() {
+        return level;
+    }
 
-        public void setArea_parent(int area_parent) {
-            this.area_parent = area_parent;
-        }
+    public void setLevel(int level) {
+        this.level = level;
+    }
 
-        public int getArea_code() {
-            return area_code;
-        }
+    public int getArea_parent() {
+        return area_parent;
+    }
 
-        public void setArea_code(int area_code) {
-            this.area_code = area_code;
-        }
+    public void setArea_parent(int area_parent) {
+        this.area_parent = area_parent;
+    }
 
-        public String getArea_name() {
-            return area_name;
-        }
+    public int getArea_code() {
+        return area_code;
+    }
 
-        public void setArea_name(String area_name) {
-            this.area_name = area_name;
-        }
+    public void setArea_code(int area_code) {
+        this.area_code = area_code;
+    }
 
-        public int getUnit_id() {
-            return unit_id;
-        }
+    public String getArea_name() {
+        return area_name;
+    }
 
-        public void setUnit_id(int unit_id) {
-            this.unit_id = unit_id;
-        }
+    public void setArea_name(String area_name) {
+        this.area_name = area_name;
+    }
 
-        public double getLat() {
-            return lat;
-        }
+    public int getUnit_id() {
+        return unit_id;
+    }
 
-        public void setLat(double lat) {
-            this.lat = lat;
-        }
+    public void setUnit_id(int unit_id) {
+        this.unit_id = unit_id;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
 }
